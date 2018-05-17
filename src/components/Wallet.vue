efd4284ec8ef4b7846a21cafa1fa429bf95447e1
刘梅花 <liumeihua@jiandan100.cn>
2018-05-17 16:01:57 +0800

    UI

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -1,40 +1,46 @@
 <template>
   <div class="container">
-    <mt-header v-bind:title="wallet && wallet.name || '钱包'">
-      <mt-button icon="more" slot="right" v-on:click="showPop"></mt-button>
-    </mt-header>
-    <div class="right">
-      <div>钱包名称: {{ wallet && wallet.name || '未命名'}}  </div>
-      <div>收款地址: {{ publicKey }}</div>
-      <p v-if="!isOnlineAccount">您的账户还未被网络记录，你需要发送至少20个lumens (XLM)到这个账户.</p>
-
-      <mt-button type="default" v-on:click="askForSomeLumens">叫爸爸</mt-button>
-      <div>
-        <mt-button type="primary" v-on:click="showWalletSend">转账</mt-button>
+    <div class="head">
+      <div class="navbar fn-flex">
+        <div class="navbar-left fn-flex">
+          <i class="iconfont icon-wallet logo"></i>
+          <h1>钱包</h1>
+        </div>
+        <div class="navbar-right">
+          <div class="navbar-right-select" v-on:click="showPop">管理<i class="iconfont icon-list"></i></div>
+          <div class="tool-slide" v-if="showTools">
+            <div class="tool-arrow"><i class="iconfont icon-arrow"></i></div>
+            <ul class="tool-list">
+              <li><router-link to="/wallet_list" class="fn-flex"><span>管理钱包</span><i class="iconfont icon-angle"></i></router-link></li>
+              <li><router-link to="/wallet_list" class="fn-flex"><span>设置</span><i class="iconfont icon-angle"></i></router-link></li>
+            </ul>
+          </div>
+        </div>
       </div>
-
-      <div>
-      <label>账户余额</label> {{ balance }} XLM <br />
-      <label>usd:</label> {{ usdBalance }}
+    </div>
+    <div class="tips" v-if="!isOnlineAccount"><p>您的账户还未被网络记录，你需要发送至少20个lumens (XLM)到这个账户。</p></div>
+    <div class="content">
+      <ul class="">
+        <li class="blue-text">{{ wallet && wallet.name || '未命名'}}</li>
+        <li>{{ balance }} XLM</li>
+        <li>{{ usdBalance }} USD</li>
+        <li><button type="button" v-on:click="askForSomeLumens" class="ui-btn">获取测试币</button></li>
+      </ul>
+      <ul class="">
+        <li>收钱</li>
+        <li>转账</li>
+      </ul>
+      <div class="">
+        <div>收款地址: {{ publicKey }}</div>
+        <div class="">
+          <WalletSend />
+        </div>
       </div>
 
       <div>
         <TransactionList v-bind:publicKey="publicKey"/>
       </div>
     </div>
-
-    <mt-popup
-      v-model="popupVisible"
-      position="bottom"
-      class="page-popup mint-popup-3"
-      popup-transition="popup-fade">
-      <router-link to="/wallet_list">
-        <mt-button class="mint-button">钱包管理</mt-button>
-      </router-link>
-      <router-link to="/setup">
-        <mt-button class="mint-button">设置</mt-button>
-      </router-link>
-    </mt-popup>
 
     <mt-popup
       v-model="walletSend"
@@ -69,22 +75,22 @@ export default {
     WalletSend
   },
   created: function () {
-    var wallet = this.wallet
-    var that = this
+    var wallet = this.wallet;
+    var that = this;
     if (!wallet) {
       return this.$router.go(-1)
     }
-    this.publicKey = wallet.publicKey
-    this.loadAccound(wallet)
+    this.publicKey = wallet.publicKey;
+    this.loadAccound(wallet);
 
     // 监听变更
-    var server = sdk.getServer()
+    var server = sdk.getServer();
     server.payments()
       .cursor('now')
       .forAccount(wallet.publicKey)
       .stream({
         onmessage: function (message) {
-          console.log(message)
+          console.log(message);
           that.loadAccound(wallet)
         }
       })
@@ -95,11 +101,11 @@ export default {
     ]),
 
     showPop: function () {
-      this.popupVisible = true
+      this.showTools = !this.showTools;
     },
     askForSomeLumens: function () {
       request.get('http://47.75.60.214:8004').query({ addr: this.publicKey }).then(function (res) {
-        Toast('好朋友给你赠送了1000个lumens，请查收!')
+        Toast('好朋友给你赠送了1000个lumens，请查收!');
         this.loadAccound()
       }).catch(function () {
         Toast('要钱没有，要命一条')
@@ -116,8 +122,8 @@ export default {
       valueType: 'Coinmarketcap',
       valueTypes: ['Coinmarketcap', 'Kraken'],
       wallets: [],
-      popupVisible: false,
-      walletSend: false
+      walletSend: false,
+      showTools: false
     }
   }
 }
@@ -125,17 +131,40 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.page-popup .mint-popup-3 {
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-}
-.page-popup .mint-popup-3 .mint-button {
-    position: absolute;
-    width: 90%;
-    top: 50%;
-    left: 5%;
-    -webkit-transform: translateY(-50%);
-            transform: translateY(-50%);
-}
+  .head{background-color: #fff; box-shadow: 0 2px 2px rgba(0,0,0,.05), 0 1px 0 rgba(0,0,0,.05); position: relative; z-index: 1;}
+  .navbar{box-sizing: border-box; height: 3.5em; max-width: 60em; padding: 0 0.625em; margin: 0 auto; line-height: 3.5em; justify-content: space-between;}
+  .navbar .logo{font-size: 1.5em; color: #2196f3;}
+  h1{margin-left: 0.35em; font-size: 1.25em;}
+  .navbar-right{position: relative; z-index: 1;}
+  .navbar-right-select i{margin-left: 0.35em; color: #999;}
+  .tool-slide{position: absolute; z-index: 10; right: -0.625em;}
+  .tool-list{width: 8em; background-color: #fff; box-shadow: 0 0 5px rgba(0,0,0,.2), 0 0 5px rgba(0,0,0,.2);}
+  .tool-arrow{position: absolute; right: 0.5em; top: -0.2em; z-index: 9; color: #fff; text-shadow: rgba(0,0,0,.25) 0 -0.07em 0.1em;font-size: 1.25em; line-height: 0;}
+  .tool-list li{border-top: 1px solid #f0f0f0; font-size: 0.875em; line-height: 150%;}
+  .tool-list li .fn-flex{justify-content: space-between; padding: 0.875em 0.625em;}
+  .tool-list li i{font-size: 0.625em; color: #9e9e9e;}
+
+  .tips{box-sizing: border-box; max-width: 60em; margin: 0 auto; padding: 0.75em;}
+  .tips p{font-size: 0.875em;}
+  .content{box-sizing: border-box; max-width: 60em; margin: 0 auto; padding: 2em; background-color: #fff;}
+
+
+
+  .page-popup .mint-popup-3 {
+      width: 100%;
+      height: 100%;
+      background-color: #fff;
+  }
+  .page-popup .mint-popup-3 .mint-button {
+      position: absolute;
+      width: 90%;
+      top: 50%;
+      left: 5%;
+      -webkit-transform: translateY(-50%);
+              transform: translateY(-50%);
+  }
+
+  @media screen and (max-width: 480px) {
+
+  }
 </style>
