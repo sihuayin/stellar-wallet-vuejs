657541bac1056cae6de2f09c42392f14f14ae071
刘梅花 <liumeihua@jiandan100.cn>
2018-05-22 15:57:36 +0800

    UI

--- src/components/WalletList.vue
+++ src/components/WalletList.vue
@@ -2,22 +2,20 @@
   <div class="container">
     <Head />
     <div class="content">
-      <h2 class="column-title">钱包账户管理</h2>
-      <ul id="example-1">
-        <li v-for="(wallet, index) in wallets" v-bind:key="index">
-          <p>{{ index }} <input type="radio" name="active_value" v-model="activeIndex" v-bind:value="index"></p>
-          <p><label>钱包名称 :</label> <input v-bind:value="wallet.name" v-on:input="updateOneWalletName($event.target.value, index)"/></p>
-          <p><label>钱包密钥 :</label> <input disabled readonly v-bind:value="wallet.secretKey" /></p>
-          <p><label>地址 :</label> <input disabled readonly v-bind:value="wallet.publicKey" /></p>
+      <div class="fn-flex column-top"><h2 class="column-title">钱包账户管理</h2><button type="button" class="ui-btn" v-on:click="showForm">添加</button></div>
+      <ul id="example-1" class="wallet-list">
+        <li class="fn-flex" v-for="(wallet, index) in wallets" v-bind:key="index">
+          <div class="checkBox"><input type="radio" name="active_value" v-model="activeIndex" v-bind:value="index"></div>
+          <div class="wallet">
+            <p><span class="label">名称：</span><input v-bind:value="wallet.name" v-on:input="updateOneWalletName($event.target.value, index)"/></p>
+            <p class="fn-break"><span class="label">密钥：</span><span>{{wallet.secretKey}}</span></p>
+            <p class="fn-break"><span class="label">地址：</span><span>{{wallet.publicKey}}</span></p>
+          </div>
         </li>
       </ul>
-
-      <mt-button type="default" v-on:click="showForm">添加</mt-button>
-      <mt-popup
-        v-model="popupVisible">
-        <AddPanel v-on:onComplete="finishAdd" />
-      </mt-popup>
+      <div class="submitBtn"><button type="button" class="ui-btn">保存</button></div>
     </div>
+    <modals-container />
   </div>
 </template>
 
@@ -53,7 +51,13 @@ export default {
       'updateWalletActive'
     ]),
     showForm: function () {
-      this.popupVisible = true
+      this.$modal.show(AddPanel, {
+        onComplate: () => {}
+      }, {
+        height: 'auto'
+      }, {
+        'before-close': (event) => { console.log('this will be called before the modal closes') }
+      })
     },
     finishAdd: function () {
       this.popupVisible = false
@@ -79,11 +83,19 @@ export default {
 </script>
 
 <style scoped>
+  .fn-break{white-space:normal; word-break:break-all;}
+  .column-top{margin-bottom: 1em; padding: 0.5em 0; justify-content: space-between;}
   .content{box-sizing: border-box; max-width: 60em; min-height: 36em; margin: 0 auto; padding: 1.5em 1em; background-color: #fff;}
-  .column-title{margin-bottom: 1em; padding: 0.5em 0; font-size: 1.125em;}
+  .column-title{font-size: 1.125em;}
+  .wallet-list{border-top: 1px solid #e5e5e5;}
+  .wallet-list li{padding: 1em 0.625em; border-bottom: 1px solid #e5e5e5;}
+  .wallet-list li .checkBox{margin:  0 1em;}
+  .submitBtn{margin-top: 2.5em; text-align: center;}
+  .submitBtn .ui-btn{width: 100%;}
 
   @media screen and (max-width: 480px) {
-    .content{padding: 1em 0 0; min-height: auto;}
-    .column-title{margin-bottom: 1em; padding: 0.5em 0.625em;}
+    .content{padding: 1em 0; min-height: auto;}
+    .column-top{padding: 0.5em 0.625em;}
+    .submitBtn{margin-left: 0.625em; margin-right: 0.625em;}
   }
 </style>
